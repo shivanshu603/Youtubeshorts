cc3dff0d5059e16ea10ecf80f622fb2576070ae7
shivanshu603 <166390746+shivanshu603@users.noreply.github.com>
2026-05-01 18:56:54 +0530

    Increase speech rate from +10% to +15%

--- modules/audio.py
+++ modules/audio.py
@@ -19,7 +19,7 @@ class AudioEngine:
                 communicate = edge_tts.Communicate(
                     text=text,
                     voice=self.voice,
-                    rate="+10%",      # Thoda fast for energy
+                    rate="+15%",      # Thoda fast for energy
                     pitch="-3Hz",    # Masculine aur deep feel
                     volume="+8%"
                 )
